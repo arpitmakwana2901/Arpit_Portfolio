ec20403d6623ff428691ca87c6c55aa40a9e0057
arpitmakwana2901 <arpitmakwana7376@gmail.com>
2026-04-10 18:50:01 +0530

    portfolio

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1k2P9wc1jzeFRXPsuLm7OFvMAjUtcmu7_/view?usp=sharing"
+            href="https://drive.google.com/file/d/1oRCsDS2IF-9S5a4O-4F5FKbxBktlgUXX/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
